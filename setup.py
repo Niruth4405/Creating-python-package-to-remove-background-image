359ff709b7c048c7749a70c00f536d73c17c9f4b
Niruth Ananth <niruthananth03@gmail.com>
2025-03-21 15:42:36 +0530

    2nd commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bg_remover",
-    version="0.1",
+    name="mymom",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         "rembg",
